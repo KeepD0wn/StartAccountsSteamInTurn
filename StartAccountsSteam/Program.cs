ba490fa7963f04c874e17231c40c66a5c34c28f8
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2021-05-15 23:25:28 +0300

    v1.3
    - добавил подписку

--- StartAccountsSteam/Program.cs
+++ StartAccountsSteam/Program.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Management;
 using System.Security.Cryptography;
+using System.Configuration;
 
 namespace StartAccountsSteam
 {
@@ -401,6 +402,45 @@ namespace StartAccountsSteam
                         key = sr.ReadToEnd();
                     }
                     key = key.Replace("\r\n", "");
+
+                    MySqlConnection conn = new MySqlConnection();
+                    try
+                    {
+                        ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+                        conn = new MySqlConnection(settings.ToString());
+                        conn.Open();
+
+                        var com = new MySqlCommand("USE `MySQL-1964`; " +
+                         "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
+                        com.Parameters.AddWithValue("@keyLic", key);
+
+                        using (DbDataReader reader = com.ExecuteReader())
+                        {
+                            if (reader.HasRows) //тут уходит на else если нет данных
+                            {
+
+                            }
+                            else
+                            {
+                                conn.Close();
+                                Console.WriteLine("[SYSTEM] License is not active");
+                                Thread.Sleep(5000);
+                                Environment.Exit(0);
+                            }
+                        }
+                        conn.Close();
+                    }
+                    catch
+                    {
+                        conn.Close();
+                        Console.WriteLine("[SYSTEM][404] Something went wrong!");
+                        Thread.Sleep(5000);
+                        Environment.Exit(0);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
 
                     if (PcInfo.GetCurrentPCInfo() == key)
                     {
